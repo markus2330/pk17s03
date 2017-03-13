137b08c323ff3d6b193a81062c1bc87d629d12fa
Markus Raab <elektra@markus-raab.org>
2017-03-13 16:27:38 +0100

    i -> I

---
+++ src/ParameterNames.java
@@ -0,0 +1,14 @@
+/**
+ * Which number do I print?
+ */
+public class ParameterNames {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        xyMinus(y, x);
+    }
+
+    private static void xyMinus(int x, int y) {
+        System.out.println(x - y);
+    }
+}
