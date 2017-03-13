4ab2189fe97c7ee834e60564e943b6398823e0b5
Markus Raab <elektra@markus-raab.org>
2017-03-13 13:45:36 +0100

    add Pizza

---
+++ src/Pizza.java
@@ -0,0 +1,18 @@
+/**
+ * Created by markus on 13.03.17.
+ */
+public class Pizza {
+
+    private static void pizzaTeig() {
+        System.out.print(500);
+        System.out.println(" g Mehl");
+        System.out.print(1);
+        System.out.println(" Würfel Germ");
+        System.out.print(250);
+        System.out.println(" ml Wasser");
+    }
+
+    public static void main(String[] args) {
+        pizzaTeig();
+    }
+}
